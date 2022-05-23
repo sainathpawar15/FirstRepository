54399b092ba69658f6566312cc58b89eba691a31
sainathpawar15 <104062418+sainathpawar15@users.noreply.github.com>
2022-05-23 07:19:08 +0530

    Add files via upload
    basic program commit

---
+++ Xyz.java
@@ -0,0 +1,14 @@
+
+ class Xyz
+{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Display d=new Display();
+		  Mythread2 t1=new Mythread2(d, "Sainath");
+		  t1.start();
+		  
+
+	}
+
+}
